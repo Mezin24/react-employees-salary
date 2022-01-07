09f69041e356e3aaa9b6ab9e313d282eaef79c7b
Pavel Mezencev <mezencev24@mail.ru>
2022-01-07 23:38:15 +0300

    change employeeForm via class

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,28 +1,34 @@
+import { Component } from 'react/cjs/react.production.min';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          placeholder="Как его зовут?"
-          className="form-control new-post-label"
-        />
-        <input
-          type="number"
-          placeholder="З/П в $?"
-          id=""
-          className="form-control new-post-label"
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            placeholder="Как его зовут?"
+            className="form-control new-post-label"
+          />
+          <input
+            type="number"
+            placeholder="З/П в $?"
+            className="form-control new-post-label"
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
