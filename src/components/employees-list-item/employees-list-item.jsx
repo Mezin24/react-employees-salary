32bd75bd674d3669e6ea535f93850e09653ea800
Pavel Mezencev <mezencev24@mail.ru>
2022-01-07 23:15:23 +0300

    add cookie btn feature

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -4,16 +4,24 @@ import './employees-list-item.css';
 class EmployeesListItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      increase: this.props.increase,
+    };
   }
 
+  increaseHandler = () => {
+    this.setState((state) => ({
+      increase: !state.increase,
+    }));
+  };
+
   render() {
-    const { name, salary, increase } = this.props;
+    const { name, salary } = this.props;
+    const itemClassName = `list-group-item d-flex justify-content-between ${
+      this.state.increase ? 'increase' : ''
+    }`;
     return (
-      <li
-        className={`list-group-item d-flex justify-content-between ${
-          increase ? 'increase' : ''
-        }`}
-      >
+      <li className={itemClassName}>
         <span className="list-group-item-label">{name}</span>
         <input
           type="text"
@@ -21,7 +29,11 @@ class EmployeesListItem extends Component {
           defaultValue={salary + '$'}
         />
         <div className="d-flex justify-content-center align-items-center">
-          <button className="btn-cookie btn-sm" type="button">
+          <button
+            className="btn-cookie btn-sm"
+            type="button"
+            onClick={this.increaseHandler}
+          >
             <i className="fas fa-cookie"></i>
           </button>
           <button className="btn-trash btn-sm" type="button">
