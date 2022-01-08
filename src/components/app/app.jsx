632efb0c83ee1e251b5d5e9c3545ff3e6a984799
Pavel Mezencev <mezencev24@mail.ru>
2022-01-08 15:42:25 +0300

    change app fn to class

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,4 @@
+import {Component} from 'react'
 import classes from './app.module.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
@@ -5,19 +6,29 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../emplyees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-const App = () => {
-  const data = [
-    { name: 'John C.', salary: 800, increase: false, id: 1 },
-    { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
-    { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
-  ];
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
+      ]
+    }
+  }
 
-  const onDelete = id => {
+
+ onDelete = id => {
     console.log(id)
   }
 
-  return (
-    <div className={classes.app}>
+  render() {
+
+    const {data} = this.state
+
+    return (
+      <div className={classes.app}>
       <AppInfo />
 
       <div className={classes['search-panel']}>
@@ -25,10 +36,11 @@ const App = () => {
         <AppFilter />
       </div>
 
-      <EmployeesList employees={data} onDelete={onDelete}/>
+      <EmployeesList employees={data} onDelete={this.onDelete}/>
       <EmployeesAddForm />
     </div>
   );
+}
 };
 
 export default App;
